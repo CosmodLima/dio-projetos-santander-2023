242bafc3314c843a65e58ed7f87a42ebfc5f8ffa
CosmodLima <cosmodlima016@gmail.com>
2023-09-01 18:43:59 -0300

    .

--- projetos-java/ContaBanco/src/ContaTerminal.java
+++ projetos-java/ContaBanco/src/ContaTerminal.java
@@ -8,30 +8,28 @@ public class ContaTerminal {
     //Exibir a menssagem conta criada
        int numeroConta = 1021;
        String agencia = "067-8";
-
-       
        double saldoConta;
        
        Scanner scanner = new Scanner(System.in);
 
         
        System.out.println("Bem vindo a Banco Cosmos o Número 1 da Galáxia.");
-       System.out.println("Digite seu nome para que possamos encontrar sua conta:" );
+       System.out.println("Digite seu nome para que possamos encontrar sua conta:");
        String nome = scanner.next();
 
 
-       System.out.println("Digite seu sobrenome:" );
+       System.out.println("Digite seu sobrenome:");
        String sobrenome = scanner.next();
 
        System.out.println("Parece que o cliente: " + nome + " " + sobrenome + " não possue conta em nosso banco.");
 
-       System.out.println("Digite sua data de nascimento para criar uma conta:" );
+       System.out.println("Digite sua data de nascimento para criar uma conta:");
        String dataNascimento = scanner.next();
     
        System.out.println("Digite o saldo inicial da sua conta:");
        saldoConta = scanner.nextDouble();
 
-        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
+       System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
       
        
     }
